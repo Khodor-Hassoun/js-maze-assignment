3a036f2ec74d6078e9270773044c2cc96283ecfc
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-04 13:11:51 +0300

    Some modification

--- game.js
+++ game.js
@@ -1,53 +1,6 @@
 document.addEventListener("DOMContentLoaded", gameLaunch)
 
 
-// let score = 0
-// isStart = false
-// const star = document.getElementById("start")
-// const end = document.getElementById("end")
-// const boundries = document.getElementsByClassName("boundary")
-// const scoreSheet = document.querySelector(".example")
-// scoreSheet.innerHTML = `score = ${score}`
-
-// //Game Start
-// star.addEventListener('click', function(){
-//     isStart = true
-//     score = 0
-//     changeColor(boundries,'#eeeeee')
-//     scoreSheet.innerHTML = `score = ${score}`
-// })
-
-// end.addEventListener('mouseover', function(){
-//     if(isStart){
-//         score = 5
-//         isStart = false
-//         changeColor(boundries,'green')
-//         scoreSheet.innerHTML = `score = ${score}`
-//     }
-// })
-// //Check boundries
-// for(boundry of boundries){
-//     boundry.addEventListener('mouseover', function(){
-//         if(isStart){
-//             score = -10
-//             isStart = false
-//             changeColor(boundries,'red')
-//             scoreSheet.innerHTML = `score = ${score}`
-
-//         }
-        
-//     })
-// }
-// function changeColor(array, color){
-//     for (let i = 0; i< array.length-1; i++){
-//             array[i].style.backgroundColor = color;
-//         }
-// }
-// // if(score == -10){
-// //     for (let i = 0; i< boundries.length-1; i++){
-// //         boundries[i].style.color = "red";
-// //     }
-// // }
 function gameLaunch(){
     let score = 0;
     isStart = false;
@@ -55,6 +8,7 @@ function gameLaunch(){
     const end = document.getElementById("end");
     const boundries = document.getElementsByClassName("boundary");
     const scoreSheet = document.querySelector(".example");
+    const container = document.getElementById("game")
     // scoreSheet.innerHTML = `score = ${score}`;
 
     //Game Start
