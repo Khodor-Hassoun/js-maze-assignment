399f2a98063bd2fb01eee630af7b7c230fb82f00
Khodor Hassoun <khodorhassoun@outlook.com>
2022-09-04 12:55:49 +0300

    Main function working

--- game.js
+++ game.js
@@ -1,61 +1,92 @@
-gameStart = false
-// document.addEventListener("DOMContentLoaded", function () {
-//     const star = document.getElementById("start")
-//     const end = document.getElementById("end")
-//     const boundries = document.getElementsByClassName("boundary")
-//     const scoreSheet = document.getElementsByClassName("example")
-//     startGame(star)
-//     loseParam(boundries)
-//     endGame(end)
-//     });
+document.addEventListener("DOMContentLoaded", gameLaunch)
 
-// function gameStart(){
 
-// }
-let score = 0
-isStart = false
-const star = document.getElementById("start")
-const end = document.getElementById("end")
-const boundries = document.getElementsByClassName("boundary")
-const scoreSheet = document.querySelector(".example")
-scoreSheet.innerHTML = `score = ${score}`
+// let score = 0
+// isStart = false
+// const star = document.getElementById("start")
+// const end = document.getElementById("end")
+// const boundries = document.getElementsByClassName("boundary")
+// const scoreSheet = document.querySelector(".example")
+// scoreSheet.innerHTML = `score = ${score}`
 
-//Game Start
-star.addEventListener('click', function(){
-    isStart = true
-    score = 0
-    changeColor(boundries,'#eeeeee')
-    scoreSheet.innerHTML = `score = ${score}`
-})
+// //Game Start
+// star.addEventListener('click', function(){
+//     isStart = true
+//     score = 0
+//     changeColor(boundries,'#eeeeee')
+//     scoreSheet.innerHTML = `score = ${score}`
+// })
 
-end.addEventListener('mouseover', function(){
-    if(isStart){
-        score = 5
-        isStart = false
-        changeColor(boundries,'green')
-        scoreSheet.innerHTML = `score = ${score}`
-    }
-})
-//Check boundries
-for(boundry of boundries){
-    boundry.addEventListener('mouseover', function(){
-        if(isStart){
-            score = -10
-            isStart = false
-            changeColor(boundries,'red')
-            scoreSheet.innerHTML = `score = ${score}`
-
-        }
-        
-    })
-}
-function changeColor(array, color){
-    for (let i = 0; i< boundries.length-1; i++){
-            boundries[i].style.backgroundColor = color;
-        }
-}
-// if(score == -10){
-//     for (let i = 0; i< boundries.length-1; i++){
-//         boundries[i].style.color = "red";
+// end.addEventListener('mouseover', function(){
+//     if(isStart){
+//         score = 5
+//         isStart = false
+//         changeColor(boundries,'green')
+//         scoreSheet.innerHTML = `score = ${score}`
 //     }
+// })
+// //Check boundries
+// for(boundry of boundries){
+//     boundry.addEventListener('mouseover', function(){
+//         if(isStart){
+//             score = -10
+//             isStart = false
+//             changeColor(boundries,'red')
+//             scoreSheet.innerHTML = `score = ${score}`
+
+//         }
+        
+//     })
 // }
+// function changeColor(array, color){
+//     for (let i = 0; i< array.length-1; i++){
+//             array[i].style.backgroundColor = color;
+//         }
+// }
+// // if(score == -10){
+// //     for (let i = 0; i< boundries.length-1; i++){
+// //         boundries[i].style.color = "red";
+// //     }
+// // }
+function gameLaunch(){
+    let score = 0;
+    isStart = false;
+    const star = document.getElementById("start");
+    const end = document.getElementById("end");
+    const boundries = document.getElementsByClassName("boundary");
+    const scoreSheet = document.querySelector(".example");
+    // scoreSheet.innerHTML = `score = ${score}`;
+
+    //Game Start
+    star.addEventListener("click", function () {
+      isStart = true;
+      score = 0;
+      changeColor(boundries, "#eeeeee");
+      scoreSheet.innerHTML = `score = ${score}`;
+    });
+
+    end.addEventListener("mouseover", function () {
+      if (isStart) {
+        score = 5;
+        isStart = false;
+        changeColor(boundries, "green");
+        scoreSheet.innerHTML = `score = ${score}`;
+      }
+    });
+    //Check boundries
+    for (boundry of boundries) {
+      boundry.addEventListener("mouseover", function () {
+        if (isStart) {
+          score = -10;
+          isStart = false;
+          changeColor(boundries, "red");
+          scoreSheet.innerHTML = `score = ${score}`;
+        }
+      });
+    }
+    function changeColor(array, color) {
+      for (let i = 0; i < array.length - 1; i++) {
+        array[i].style.backgroundColor = color;
+      }
+    }
+}
